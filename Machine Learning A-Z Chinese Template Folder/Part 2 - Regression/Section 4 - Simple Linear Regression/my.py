3609586bac5eb3526aaa643bf3fffe87f348fbc7
victor <fengxuanwang@qq.com>
2018-12-06 22:02:14 +0800

    a

--- Machine Learning A-Z Chinese Template Folder/Part 2 - Regression/Section 4 - Simple Linear Regression/my.py
+++ Machine Learning A-Z Chinese Template Folder/Part 2 - Regression/Section 4 - Simple Linear Regression/my.py
@@ -5,3 +5,22 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 dataset = pd.read_csv('Salary_Data.csv')
+
+X = dataset.iloc[:, :-1].values;
+y = dataset.iloc[:, 1].values;
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+y_pred = regressor.predict(X_test)
+
+plt.scatter(X_train, y_train, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary VS Experience (training set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
