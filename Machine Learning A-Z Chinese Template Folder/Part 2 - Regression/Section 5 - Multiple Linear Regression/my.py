6aaa87980c28060d2af5aaddbfbe81037fae8e2d
victor <fengxuanwang@qq.com>
2018-12-13 21:57:57 +0800

    polynomial regression

--- Machine Learning A-Z Chinese Template Folder/Part 2 - Regression/Section 5 - Multiple Linear Regression/my.py
+++ Machine Learning A-Z Chinese Template Folder/Part 2 - Regression/Section 5 - Multiple Linear Regression/my.py
@@ -12,3 +12,44 @@ y = dataset.iloc[:, -1].values
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_X = LabelEncoder()
 X[:, 3] = labelencoder_X.fit_transform(X[:, 3])
+onehotencoder = OneHotEncoder(categorical_features = [3])
+X = onehotencoder.fit_transform(X).toarray()
+
+X = X[:, 1:]
+
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+y_pred = regressor.predict(X_test)
+
+import statsmodels.formula.api as sm
+X_train = np.append(arr = np.ones((40, 1)), values = X_train, axis = 1)
+
+X_opt = X_train[:, [0,1,2,3,4,5]]
+regressor_OLS = sm.OLS(endog = y_train, exog = X_opt).fit()
+regressor_OLS.summary()
+
+
+X_opt = X_train[:, [0,1,3,4,5]]
+regressor_OLS = sm.OLS(endog = y_train, exog = X_opt).fit()
+regressor_OLS.summary()
+
+
+X_opt = X_train[:, [0,3,4,5]]
+regressor_OLS = sm.OLS(endog = y_train, exog = X_opt).fit()
+regressor_OLS.summary()
+
+
+X_opt = X_train[:, [0,3,5]]
+regressor_OLS = sm.OLS(endog = y_train, exog = X_opt).fit()
+regressor_OLS.summary()
+
+
+X_opt = X_train[:, [0,3]]
+regressor_OLS = sm.OLS(endog = y_train, exog = X_opt).fit()
+regressor_OLS.summary()
